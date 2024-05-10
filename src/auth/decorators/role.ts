866033d5fcd1de23fd32302a01ec2a74e7f3054a
Elie-MK <elie.mk25@gmail.com>
2024-05-10 11:31:07 +0100

    added role authorization and refactor genderEnum

---
+++ src/auth/decorators/role.ts
@@ -0,0 +1,6 @@
+import { SetMetadata } from "@nestjs/common";
+import { RoleEnum } from "../Enums/roleEnum"; 
+
+export const ROLES_KEY = 'role';
+export const Role = (role:RoleEnum)=>SetMetadata(ROLES_KEY, role) 
+
